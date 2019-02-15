55ad722bed79bc688ac8d78c74b0dd06a595f12c
mv04 <mtvickery1@gmail.com>
2019-02-15 14:31:09 -0600

    forEach to set defender and character objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
 // Global Variables
 var characterSelected = false;
 var defenderSelected = false;
+
 var enemies;
 var selectedCharacter;
+var defender;
 
 // Character Constructor
 function Character(name, health, damage) {
@@ -16,6 +18,7 @@ var amanda = new Character("amanda", 100, 14);
 var kate = new Character("kate", 120, 8);
 var ricardo = new Character("ricardo", 150, 8);
 var mason = new Character("mason", 180, 7);
+var characterArray = [amanda, kate, ricardo, mason];
 
 // Health
 var amandaHealthDiv = $("#amanda-health");
@@ -44,6 +47,14 @@ $(".character").on("click", function () {
     $(selectedCharacter).appendTo("#your-character");
     characterSelected = true;
 
+    // Make selectedCharacter = selectedCharacterObject
+    selectedCharacter = $(this).children()[1].name;
+    characterArray.forEach(function (characterObject) {
+      if (characterObject.name === selectedCharacter) {
+        selectedCharacter = characterObject;
+      }
+    });
+
     // Hide Character Select Section
     $("#select-character-container").attr("class", "hide");
 
@@ -70,6 +81,16 @@ $("#enemies").on("click", ".enemy", function () {
 
     defenderSelected = true;
 
+    // Sets defender = chosen defender
+    defender = $(this).children()[1].name;
+    characterArray.forEach(function (defenderObject) {
+      if (defenderObject.name === defender) {
+        console.log(defender);
+        defender = defenderObject;
+      }
+    });
+
+
     // Call a function to begin fighting...or not, make on click
   }
 });
@@ -79,23 +100,38 @@ $("#attack-button").on("click", function () {
 
   if (defenderSelected === true) {
 
-  console.log("attack");
+    console.log("attack");
 
-  // #selected-character attacks .defender
-  // display enemy attacked
-  // update then display damage done
-  // update then display new enemy health
-  // check if defender health is === 0...
-  // if so, check if defendersDefeated === 3...
-  // if so, you win
 
-  // .defender attacks #selected-character
-  // update then display damage done to you
-  // update then display your new health
-  // check if your health is === 0...
-  // if so, you lose
+    console.log('defender:', defender)
 
-  // 
+    // #selected-character attacks .defender
+    attackDefender()
+    // update damage done
+    // updateDamage()
+    // // display damage done
+    // displayDamage()
+    // // update new enemy health
+    // updateHealth()
+    // // display new enemy health
+    // displayHealth()
+    // // display enemy attacked
+    // // check if defender health is === 0...
+    // // if so, check if defendersDefeated === 3...
+    // // if so, you win
+
+    // // .defender attacks #selected-character
+    // defenderAttacks()
+    // update then display your new health
+    // update then display damage done to you
+    // check if your health is === 0...
+    // if so, you lose
 
   }
 })
+
+function attackDefender() {
+  console.log('defender health before attack:', defender.health)
+  defender.health = defender.health - selectedCharacter.damage
+  console.log('defender health after attack:', defender.health)
+}
