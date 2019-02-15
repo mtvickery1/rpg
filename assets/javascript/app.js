0b8b66374264c74aac436e9b8202fb523991099d
mv04 <mtvickery1@gmail.com>
2019-02-15 12:43:43 -0600

    set up function to move .enemy to #defender

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 // Global Variables
 var characterSelected = false;
+var defenderSelected = false;
 var enemies;
 var selectedCharacter;
 
@@ -29,26 +30,48 @@ masonHealthDiv.text(mason.health);
 
 // Character/Enemy Selection
 $(".character").on("click", function () {
-  
+
+  // Prevents multiple characters from being selected
   if (characterSelected === false) {
     selectedCharacter = this;
     $(selectedCharacter).attr("id", "selected-character");
 
     console.log('selectedCharacter:', selectedCharacter)
-    
+
     // Set enemies var before moving #selected-character
     enemies = $(this).siblings()
     console.log('enemies:', enemies)
 
     // Move #selected-character to #your-character
-    $(this).appendTo("#your-character");
+    $(selectedCharacter).appendTo("#your-character");
     characterSelected = true;
-    // Hide Character Select
+
+    // Hide Character Select Section
     $("#select-character-container").attr("class", "hide");
 
     // Move enemies to #enemies
     $(enemies).appendTo("#enemies");
     // Add enemies class
-    $("#enemies").children().addClass("enemies");
+    $("#enemies").children().addClass("enemy");
+  }
+});
+
+// Choose Enemy to Attack
+$("#enemies").on("click", ".enemy", function () {
+
+  console.log("clicked")
+
+  // Prevents multiple defenders from being selected
+  if (defenderSelected === false) {
+
+    // Moving to defender div
+    $(this).appendTo("#defender");
+
+    // Adding defender class for blue border
+    $(this).removeClass("enemy").addClass("defender");
+
+    defenderSelected = true;
+
+    // Call a function to begin fighting...or not, make on click
   }
 });
